fe894049cd362c425276656832d6936fdcfc1765
Alexander A. Kapralov <alnkapa@gmail.com>
2024-10-05 17:12:21 +0300

    ready

--- reducer_variance.cpp
+++ reducer_variance.cpp
@@ -7,22 +7,22 @@ main(int argc, char **argv)
 {
     size_t count{0};
     std::string line;
-    int totValue{0};
+    double total_variance_sum = 0.0;
     int value{0};
-    int key{0};
+    int mean_price{0};
     auto max = std::numeric_limits<std::streamsize>::max();
     while (!std::cin.fail() || !std::cin.eof())
     {
         std::cin >> value;
         std::cin.ignore(max, '\t');
-        std::cin >> key;
+        std::cin >> mean_price;
         std::cin.ignore(max, '\n');
-        totValue += value;
+        total_variance_sum += (value - mean_price) * (value - mean_price);
         count += 1;
     }
     if (count > 0)
     {
-        std::cout << static_cast<float>(totValue) / count << std::endl;
+        std::cout << static_cast<double>(total_variance_sum) / count << std::endl;
     }
     else
     {
